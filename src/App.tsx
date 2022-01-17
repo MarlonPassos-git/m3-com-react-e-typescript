2fbad59f881b0bd491c359969d11e1f23f4b4280
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-16 22:16:30 -0300

    criando base do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "the-new-css-reset/css/reset.css"
+import { Cabecario } from  "./components/Cabecario"
+import { EstilosGlobais } from "./estilos/estilosGlobais"
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Cabecario />
+      <EstilosGlobais />
+    </>
+   
+  )
+  
 }
 
-export default App;
