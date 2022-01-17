11bfdb76cda2ae17cf5adb43ad924e42f5aa7462
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-16 22:16:55 -0300

    adicionando o componente cabeçario

---
+++ src/components/Cabecario/estilos.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+     display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  height: 4rem;
+  padding: 0 var(--spacing-container-padding);
+  border-bottom: 0.125rem solid var(--gray-100);
+   
+    
+`
+
+export const Img = styled.img`
+
+`
