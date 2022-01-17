13b6f5833bc61fcfa32a6454f98abb5436dce00e
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-16 22:29:02 -0300

    ➕ Componente Footer

---
+++ src/components/RodaPe/estilo.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 2.063rem;
+    width: 100%;
+    background-color: var(--black);
+    padding: 0 var(--spacing-container-padding);
+`
+
+export const Paragrafo = styled.p`
+    text-align: center;
+    color: var(--white);
+    font-size: var(--font-very-small);
+    text-transform: uppercase;
+`
---
+++ src/components/RodaPe/index.tsx
@@ -0,0 +1,12 @@
+import { Container, Paragrafo } from "./estilo";
+
+export function RodaPe() {
+
+    return (
+        <Container>
+            <Paragrafo>
+                M3: Implantação de E-commerce VTEX
+            </Paragrafo>
+        </Container>
+    )
+}
