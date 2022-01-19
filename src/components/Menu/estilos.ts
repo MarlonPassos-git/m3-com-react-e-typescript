af342c18c0433541122af6d1cc204d6a42348aa0
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-19 11:37:11 -0300

    ➕ compenente de 'Menu'

---
+++ src/components/Menu/estilos.ts
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+    display: grid;
+    grid-template:
+        "title order" 9.313rem
+        "filters produtsList"
+        ". button"
+        / 13.2088rem 1fr;
+    column-gap: 2.75rem;
+
+    min-height: calc(100vh - 4rem - 2.063rem);
+    padding: 0 var(--spacing-container-padding);
+
+    @media (max-width: 750px) {
+        grid-template:
+        "title title"
+        "filtrar ordenar "
+        "produtsList produtsList"
+        "button button"/1fr 1fr;
+
+
+        -moz-column-gap: 0;
+        column-gap: 0;
+    }
+
+`
+
+interface WrapperInputsProps {
+    tipoInput?: string;
+}
+
+export const WrapperInputs = styled.div<WrapperInputsProps>`
+    ${({ tipoInput }) => (tipoInput === 'tamanho' ) && 
+        `
+        display: flex;
+        flex-wrap: wrap;
+        gap: 0.5rem;
+        margin-bottom: 1.5rem;
+        max-width: 157px;
+        `
+    }}
+`
+
+export const Titulo = styled.h1`
+    grid-area: title;
+
+    margin-top: 2.5rem;
+
+    font-size: var(--font-very-large);
+    color: var(--black);
+
+    @media (max-width: 750px) {
+        text-align: center;
+        margin-bottom: 0.75rem;
+    }
+`
+
+export const Aside = styled.aside`
+    grid-area: filters;
+`
+
+
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { BotaoMostraMaisItens } from "../BotaoMostrarMaisItens";
+import { BotoesModal } from "../BotoesModal";
+import { ControladorOrdem } from "../ControladorOrdem";
+import { ControleFiltros } from "../ControleFiltros";
+import { ListaProdutos } from "../ListaProdutos";
+import { Aside, Container, Titulo } from "./estilos";
+
+export function Menu () {
+    const tipoTela = window.innerWidth > 768 ? false : true;
+    const [Mobile, SetMobile] = useState(tipoTela);
+
+
+    useEffect(() => {
+        window.addEventListener("resize", ()=> {
+
+
+            window.innerWidth <= 750 ? SetMobile(true) : SetMobile(false);
+        }) 
+    }, [])
+
+    return (
+        <Container>
+            <Titulo>
+                Blusas
+            </Titulo>
+            <ListaProdutos/>
+            {(Mobile) ? (<>
+                <BotoesModal
+                    gridArea="filtrar"
+                    titulo="Filtrar"
+                >
+                    <ControleFiltros/>
+                </BotoesModal>
+                <BotoesModal
+                    gridArea="ordenar"
+                    titulo="Ordenar"
+                >
+                    <ControladorOrdem />
+                </BotoesModal>
+            </>) : (<>  
+                <ControladorOrdem />
+                <Aside>
+                    <ControleFiltros/>
+                </Aside>
+                
+            </>)}
+            
+            <BotaoMostraMaisItens />
+        </Container>
+    )
+}
