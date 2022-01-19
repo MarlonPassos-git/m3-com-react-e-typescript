fcae5ca8998ce300e63b996f742d599dd95541d4
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-19 11:39:26 -0300

    ➕ provider e menu no App

--- src/Paginas/App.tsx
+++ src/Paginas/App.tsx
@@ -1,14 +1,19 @@
 import "the-new-css-reset/css/reset.css"
 import { Cabecario } from  "../components/Cabecario"
+import { Menu } from "../components/Menu"
 import { RodaPe } from "../components/RodaPe"
+import { DadosProvider } from "../context/dados"
 import { EstilosGlobais } from "../estilos/estilosGlobais"
 
 export function App() {
   return (
     <>
-      <Cabecario />
-      <RodaPe />
-      <EstilosGlobais />
+      <DadosProvider>
+        <Cabecario />
+        <Menu />
+        <RodaPe />
+        <EstilosGlobais />
+      </DadosProvider>
     </>
    
   )
